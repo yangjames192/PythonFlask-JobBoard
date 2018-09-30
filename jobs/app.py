a99467471e4a429f44fb6021513053bb9dcca3b0
yangjames192 <yangjames1992@gmail.com>
2018-09-30 09:35:34 -0400

    module5: minor fix

--- jobs/app.py
+++ jobs/app.py
@@ -36,7 +36,7 @@ def close_connection(exception):
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description,
-     job.salary, employer.id as employer_id, employer.name as employer_name
-     FROM job JOIN employer ON emplyer.id = job.emplyer_id')
+    job.salary, employer.id as employer_id, employer.name as employer_name
+    FROM job JOIN employer ON emplyer.id = job.emplyer_id')
 
-    return render_template('index.html', jobs)
+    return render_template('index.html', jobs=jobs)
