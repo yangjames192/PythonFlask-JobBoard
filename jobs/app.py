5e067f13e946714b4be486b10af65213c434f8d8
yangjames192 <yangjames1992@gmail.com>
2018-09-30 09:27:46 -0400

    module5: display all jobs

--- jobs/app.py
+++ jobs/app.py
@@ -35,4 +35,8 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description,
+     job.salary, employer.id as employer_id, employer.name as employer_name
+     FROM job JOIN employer ON emplyer.id = job.emplyer_id')
+
+    return render_template('index.html', jobs)
