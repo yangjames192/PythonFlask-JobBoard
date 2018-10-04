2c369384607cc00dbd0350b9858ce37aacc7f5f6
james-yang <james.yang@thomsonreuters.com>
2018-10-04 09:20:22 -0400

    module7: employer reviews

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template, g
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 
 PATH = 'db/jobs.sqlite'
 
@@ -57,3 +58,19 @@ def employer(employer_id):
     [employer_id])
 
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+        (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id)
